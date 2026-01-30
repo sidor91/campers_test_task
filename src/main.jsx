0c6d1196e85096f73696b8cf1294828a92031049
serhiisydorenko-wdl <sidorchik91@gmail.com>
2026-01-30 18:41:16 +0100

    Create styles.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
